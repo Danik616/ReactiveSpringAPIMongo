cacd746141b0295b2e5db2ea449bea2500e89f7c
Danik616 <ddanielvargas2011@gmail.com>
2023-05-02 12:21:35 -0500

    Se hace la creación del proyecto base

---
+++ src/test/java/com/api/rest/mongo/repaso/RepasoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.rest.mongo.repaso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RepasoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
